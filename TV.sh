83830d31fa015a44c434b3201877182000ae1e94
Gaute Holmin <gaute@topdogs.no>
2021-06-14 14:37:00 +0200

    Update TV.sh

--- TV.sh
+++ TV.sh
@@ -1,6 +1,6 @@
 # Shell script to turn on a TV and put the Raspberry Pi as active HDMI input by using CEC commands
 # First you need to install cec-utils. You can use this command: sudo apt install cec-utils
-# If you want this script to run periodically to prevent the TV from turning off, just create a CRON job that runs this script every hour or something like tha
+# If you want this script to run periodically to prevent the TV from turning off, just create a CRON job that runs this script every hour or something like that.
 # Turn TV on:
 echo 'on 0' | cec-client -s -d 1
 # Change to Raspberry Pi as active input
